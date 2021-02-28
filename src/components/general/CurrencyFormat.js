fbf5ca84d007dcb4f09783bbfe239c3c97800ac8
Melroy Dsouza <dsouzamelroy2007@gmail.com>
2021-02-28 20:41:42 +0530

    Add currency information in product card

--- src/components/general/CurrencyFormat.js
+++ src/components/general/CurrencyFormat.js
@@ -13,8 +13,25 @@ const currencyCodeLocaleMap = {
   INR: 'en-IN',
 };
 
-function CurrencyFormat({}) {
-  return null;
+
+function CurrencyFormat({
+  className,
+  value,
+  currencyCode,
+  iconSize = 1,
+  ...remainingProps
+}) {
+  return (
+    <div
+      className={classNames('currency-format', { [className]: className })}
+      {...remainingProps}
+    >
+      <Icon size={iconSize} path={currenyCodeIconMap[currencyCode]} />
+      <span className="currency-format__value">
+        {value.toLocaleString(currencyCodeLocaleMap[currencyCode])}
+      </span>
+    </div>
+  );
 }
 
 export default CurrencyFormat;
