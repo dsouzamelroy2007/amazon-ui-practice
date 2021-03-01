0bfcf87c4008d3931b8673b37b149d8b09d54e11
Melroy Dsouza <dsouzamelroy2007@gmail.com>
2021-03-01 23:46:15 +0530

    product page added with basic information

--- src/api/products.js
+++ src/api/products.js
@@ -4,7 +4,12 @@ function getProductsList() {
   return apiRequest('GET', '/products');
 }
 
+function getProduct(id){
+  return apiRequest('GET', 'products/' + id);
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getList: getProductsList,
+  getProduct: getProduct
 };
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,10 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import ImageSlider from '../../components/product/ImageSlider';
 import Information from '../../components/product/Information';
+import productsApi from 'api/products';
+import Loader from '../../components/general/Loader';
+import Error from '../../components/general/Error'
+import './index.scss';
 
 function ProductPage() {
-  return null;
+  let { productId } = useParams();
+
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const loadProduct = useCallback(async () => {
+    if(loading || product) return;
+
+    setLoading(true);
+    setError(null);
+
+    try{
+        const json = await productsApi.getProduct(productId);
+        console.log(json);
+        setProduct(json);
+    }catch(e){
+      setError(e);
+    }
+    setLoading(false);
+  }, [loading, product, productId]);
+  
+  useEffect(() => {
+    loadProduct();
+  }, [loadProduct]);
+
+  if(loading) {
+    return <Loader size = {50} />;
+  } else if(error) {
+    return <Error message = "Failed to load products" actionFn = {loadProduct} />;
+  } else if (product) {
+    return (
+      <div className="product">
+        <Information product={product} />
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default ProductPage;
